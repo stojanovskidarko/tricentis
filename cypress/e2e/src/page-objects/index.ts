7f49090d39c419e8d89653492ca0f84d468eccca
stojanovskidarko <stojanovski.darko10@gmail.com>
2024-04-26 13:03:35 +0200

    initializing an index.ts file for easier exports/imports

---
+++ cypress/e2e/src/page-objects/index.ts
@@ -0,0 +1,6 @@
+export * from './basePageObject';
+export * from './registerPage';
+export * from './loginPage';
+export * from './homePage';
+export * from './shoppingCartPage';
+export * from './productDetailsPage';
